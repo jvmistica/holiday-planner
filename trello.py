716081115c84fc5ff3c145965240e50f342279e1
Joann Mistica <joann@joannmistica.com>
2019-11-30 00:49:03 +0800

    Added methods to create boards, lists, and cards.

--- trello.py
+++ trello.py
@@ -2,19 +2,34 @@ import requests
 key = "key"
 token = "token"
 
-# Create board
-url = "https://api.trello.com/1/boards/"
-querystring = {"name": "Holidays_Test", "idBoard": "holidays", "key": key, "token": token}
-response = requests.request("POST", url, params=querystring)
 
-# Create list
-url = "https://api.trello.com/1/boards/{boardId}/lists"
-querystring = {"name": "test12", "pos": "top", "key": key, "token": token}
-response = requests.request("POST", url, params=querystring)
-print(response.text)
+def create_board(board_name):
+    url = "https://api.trello.com/1/boards/"
+    querystring = {"name": board_name, "key": key, "token": token}
+    response = requests.request("POST", url, params=querystring)
+    board_id = response.json()["shortUrl"].split("/")[-1].strip()
+    return board_id
 
-# Create card
-url = "https://api.trello.com/1/cards"
-querystring = {"name": "sample", "idList": "listId", "keepFromSource": "all","key": key, "token": token}
-response = requests.request("POST", url, params=querystring)
-print(response.text)
+
+def create_list(board_id, list_name):
+    url = f"https://api.trello.com/1/boards/{board_id}/lists"
+    querystring = {"name": list_name, "key": key, "token": token}
+    response = requests.request("POST", url, params=querystring)
+    list_id = response.json()["id"]
+    return list_id
+
+
+def create_card(list_id, card_name):
+    url = f"https://api.trello.com/1/cards"
+    querystring = {"name": card_name, "idList": list_id, "key": key, "token": token}
+    response = requests.request("POST", url, params=querystring)
+    card_id = response.json()["id"]
+    return card_id
+
+
+# Sample usage
+board_id = create_board("Holidays")
+short_list = create_list(board_id, "Short")
+medium_list = create_list(board_id, "Medium")
+long_list = create_list(board_id, "Long")
+create_card(short_list, "3 days")
