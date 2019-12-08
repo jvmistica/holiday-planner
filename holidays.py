84fc2e0dda4908f1f2be78033205345fbdcb40b1
Joann Mistica <joann@joannmistica.com>
2019-12-08 15:38:54 +0800

    Added suggested VLs.

--- holidays.py
+++ holidays.py
@@ -3,6 +3,7 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, date, timedelta
+from trello import create_board, create_list, create_card
 
 
 def scrape_holidays(url, area):
@@ -87,6 +88,26 @@ for from_date1, to_date1 in pairs:
             if leaves <= 5: # the lesser, the better
                 vacation_days = (to_date1 - from_date1).days + (to_date2 - from_date2).days + leaves
                 if vacation_days - leaves > 1:
-                    suggestions.append((vacation_days, leaves, from_date1, to_date2))
+                    suggestions.append((vacation_days + 1, leaves, from_date1.strftime("%m/%d"), to_date2.strftime("%m/%d")))
 
-print(suggestions)
+board_id = create_board("Holidays")
+first_four = create_list(board_id, "Jan - Apr", 1)
+second_four = create_list(board_id, "May - Aug", 2)
+last_four = create_list(board_id, "Sep - Dec", 3)
+suggestions_list = create_list(board_id, "Suggested Leaves", 4)
+
+# Get free time that requires no VLs
+long_weekends = get_free_time(weekends, holidays)[1]
+for from_date, to_date in long_weekends:
+    vacation_days = (to_date - from_date).days + 1
+    if from_date.month <= 4:
+        create_card(first_four, "{0} days: {1} - {2}".format(vacation_days, from_date.strftime("%m/%d"), to_date.strftime("%m/%d")))
+    elif 5 <= from_date.month <= 8:
+        create_card(second_four, "{0} days: {1} - {2}".format(vacation_days, from_date.strftime("%m/%d"), to_date.strftime("%m/%d")))
+    else:
+        create_card(last_four, "{0} days: {1} - {2}".format(vacation_days, from_date.strftime("%m/%d"), to_date.strftime("%m/%d")))
+
+# Get suggestions on when to file VLs
+suggestions.sort(reverse=True)
+for suggestion in suggestions:
+    create_card(suggestions_list, "{0} days: {2} - {3} | {1} VL".format(*suggestion))
--- trello.py
+++ trello.py
@@ -1,19 +1,19 @@
 import requests
-key = "key"
-token = "token"
+key = "your_key"
+token = "your_token"
 
 
 def create_board(board_name):
     url = "https://api.trello.com/1/boards/"
-    querystring = {"name": board_name, "key": key, "token": token}
+    querystring = {"name": board_name, "prefs_background": "sky", "key": key, "token": token}
     response = requests.request("POST", url, params=querystring)
     board_id = response.json()["shortUrl"].split("/")[-1].strip()
     return board_id
 
 
-def create_list(board_id, list_name):
+def create_list(board_id, list_name, pos):
     url = f"https://api.trello.com/1/boards/{board_id}/lists"
-    querystring = {"name": list_name, "key": key, "token": token}
+    querystring = {"name": list_name, "pos": pos, "key": key, "token": token}
     response = requests.request("POST", url, params=querystring)
     list_id = response.json()["id"]
     return list_id
@@ -25,11 +25,3 @@ def create_card(list_id, card_name):
     response = requests.request("POST", url, params=querystring)
     card_id = response.json()["id"]
     return card_id
-
-
-# Sample usage
-board_id = create_board("Holidays")
-short_list = create_list(board_id, "Short")
-medium_list = create_list(board_id, "Medium")
-long_list = create_list(board_id, "Long")
-create_card(short_list, "3 days")
